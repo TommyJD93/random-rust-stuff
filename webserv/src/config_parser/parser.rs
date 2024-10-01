b046da5f15082ed8453eb0f1484334e7f93b6f80
Tommaso Terribili <tterribili@MacBook-Air-di-aba.local>
2024-10-01 19:48:43 +0200

    starting tokenization sys

--- webserv/src/config_parser/parser.rs
+++ webserv/src/config_parser/parser.rs
@@ -5,16 +5,29 @@ pub enum ConfigError {
 	EmptyFile,
 }
 
-pub fn first_file_validation(config: String) -> Result<usize, ConfigError> {
+pub fn first_file_validation(config: String) -> Result<Vec<String>, ConfigError> {
 	let lines: Vec<&str> = config.lines().collect();
 
 	if lines.is_empty() {
 		return Err(ConfigError::EmptyFile);
 	}
 
-	
+	let mut line_num = 0;
 
-	Ok(lines.len())
+	for line in lines {
+		line_num += 1;
+		if let Some(last_char) = line.chars().last() {
+			if last_char != '}' && last_char != ';' && last_char != '{' {
+				return Err(ConfigError::InvalidLine(line_num, line.trim().to_string()));
+			}
+		}
+	}
+
+	// this requires a better way to convert the &str vec to a String vec
+	let lines: Vec<&str> = config.lines().collect();
+	let lines: Vec<String> = lines.into_iter().map(|line| line.to_string()).collect();
+
+	Ok(lines)
 }
 
 pub fn read_file(path: String) -> String {
@@ -22,8 +35,8 @@ pub fn read_file(path: String) -> String {
 		.expect("[WebServ]: Unable to open file");
 
 	match first_file_validation(config.clone()) {
-		Ok(count) => {
-			println!("[WebServ]: number of lines: {}", count);
+		Ok(conf_lines) => {
+			println!("[WebServ]: number of lines: {}", conf_lines.len());
 			return config;
 		}
 		Err(ConfigError::InvalidLine(line, content)) => {
---
+++ webserv/target/debug/deps/webserv-2e85fc6bae0396b0.d
@@ -0,0 +1,7 @@
+/Users/tterribili/Desktop/random-rust-stuff/webserv/target/debug/deps/webserv-2e85fc6bae0396b0: src/main.rs src/config_parser.rs src/config_parser/parser.rs
+
+/Users/tterribili/Desktop/random-rust-stuff/webserv/target/debug/deps/webserv-2e85fc6bae0396b0.d: src/main.rs src/config_parser.rs src/config_parser/parser.rs
+
+src/main.rs:
+src/config_parser.rs:
+src/config_parser/parser.rs:
